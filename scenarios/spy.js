c3bd0607bf0a35ed6c0108a1795aa9d78cbd1f48
ArGJolan <argjolan@gmail.com>
2019-03-12 23:22:33 +0100

    Spy scenario improvement - Can now spy a range of system from a list of planets

--- scenarios/spy.js
+++ scenarios/spy.js
@@ -28,8 +28,98 @@ class Spy extends Scenario {
     })
   }
 
-  async parseReports (page) {
+  /**
+   * Parse all spy reports in current page
+   * @param {Object} page - Puppeteer page object
+   */
+  async parseSpyReports (page) {
+    // TODO
+  }
 
+  /**
+   * Spy all inactive players in current page
+   * @param {Object} page - Puppeteer page object
+   */
+  async spyCurrentSystem (page) {
+    const inactives = await page.$$eval('.inactive_filter:not(.vacation_filter) .position', el => el.innerText)
+
+    for (let position of inactives) {
+      await this.waitUntilFleetAvailable()
+      await page.click(`[rel="planet${position}"] .ListImage a img`)
+      this.availableFleets--
+      setTimeout(() => {
+        this.availableFleets++
+      }, 90000)
+      await this.sleep(3500)
+    }
+  }
+
+  /**
+   * Spy all inactive players in range
+   * @param {Object} page - Puppeteer page object
+   * @param {Planet} planet - Planet from which probes are to be sent
+   * @param {Number} systemDelta - Spy system at this range
+   * @param {Number} maxFleets - Max number of fleets
+   */
+  async spyFromPlanet (page, planet, systemDelta, maxFleets) {
+    this.availableFleets = maxFleets
+    await planet.forcePlanet(page)
+    await this.forcePage(page, 'galaxy')
+
+    const { system } = planet.getCoordinates()
+
+    for (let currentSystem = system - systemDelta; currentSystem <= system + systemDelta; currentSystem++) {
+      await page.type('#system_input', `${currentSystem}`)
+      await this.sleep(200)
+      await page.click('#galaxyHeader .btn_blue')
+      await this.sleep(2500)
+      await this.spyCurrentSystem(page)
+    }
+  }
+
+  /**
+   * @param {Object} page - Puppeteer page object
+   * @param {Array} planetsCoordinates - Array of Strings (['1:332:10', '2:101:14' ...])
+   */
+  async action (page, planetsCoordinates, systemDelta, maxFleets) {
+    // Spy from each provided planet
+    for (let coordinates of planetsCoordinates) {
+      let planet = null
+
+      // Find the planet by its coordinates
+      for (let planetId of Object.keys(this.planets)) {
+        if (this.planets[planetId].isCoordinates(coordinates)) {
+          planet = this.planets[planetId]
+        }
+      }
+
+      if (planet) {
+        await this.spyFromPlanet(page, planet, systemDelta, maxFleets)
+      } else {
+        console.error('Could not find planet', coordinates)
+      }
+    }
+
+    // Wait till every probe is back
+    await this.sleep(120000)
+
+    await this.forcePage(page, 'messages')
+
+    const messagePaginationString = await page.$eval('#fleetsgenericpage > ul > ul:nth-child(1) > li.curPage', el => {
+      return el.innerText
+    })
+    let [currentPage, maxPage] = messagePaginationString.split('/').map(page => +page)
+    let spyReports = []
+
+    while (currentPage <= maxPage) {
+      const currentPageSpyReports = await this.parseSpyReports(page)
+      spyReports = [...spyReports, ...currentPageSpyReports]
+      if (currentPage !== maxPage) {
+        await page.click('#fleetsgenericpage > ul > ul:nth-child(1) > li:nth-child(4)')
+        await this.sleep(3500)
+      }
+      currentPage++
+    }
   }
 
   async loop (page) {
@@ -56,7 +146,7 @@ class Spy extends Scenario {
     // }
 
     // await this.sleep(120000)
-    // await this.parseReports(page)
+    // await this.parseSpyReports(page)
     // check messages & fill this.reports
   }
 
