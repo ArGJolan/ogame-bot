f807fc27ef481cd28456bbc0f83058f052e34370
ArGJolan <argjolan@gmail.com>
2019-03-19 20:42:01 +0100

    Raid scnario gg go top 1

--- scenarios/raid.js
+++ scenarios/raid.js
@@ -18,7 +18,7 @@ class Raid extends Scenario {
   async filterOutUninterestingReports (page) {
     for (let i = 0; i < 50; i++) {
       await this.forcePage(page, 'messages', true)
-      await this.sleep(5000)
+      await this.sleep(2500)
       await page.click('input[name=delEspLoot]')
       await this.sleep(500)
       await page.click('input[name=delEspDef]')
@@ -34,12 +34,59 @@ class Raid extends Scenario {
     }
   }
 
-  async action (page, { maxFleets }) {
-    if (!maxFleets) {
-      throw new Error('maxFleets needs to be set')
+  getDistance (planet, target) {
+    const [galaxyFrom, systemFrom, positionFrom] = planet.replace(/\[|\]/g, '').split(':')
+    const [galaxyTo, systemTo, positionTo] = target.replace(/\[|\]/g, '').split(':')
+    return Math.abs(galaxyFrom - galaxyTo) * 100000 + Math.abs(systemFrom - systemTo) * 100 + Math.abs(positionFrom - positionTo)
+  }
+
+  getClosestPlanet (planetList, target) {
+    let minDistance = 9999999
+    let bestPlanet = null
+    for (let planet of planetList) {
+      const distance = this.getDistance(planet, target)
+      if (distance < minDistance) {
+        minDistance = distance
+        bestPlanet = planet
+      }
     }
 
+    for (let planetId of Object.keys(this.planets)) {
+      if (this.planets[planetId].isCoordinates(bestPlanet)) {
+        return this.planets[planetId]
+      }
+    }
+  }
+
+  async action (page, { maxFleets, planetsCoordinates }) {
+    if (!maxFleets || maxFleets < 0 || !planetsCoordinates) {
+      throw new Error('maxFleets && planetsCoordinates need to be set')
+    }
+
+    let spyCount = 1
     await this.filterOutUninterestingReports(page)
+    while (maxFleets) {
+      try {
+        await this.forcePage(page, 'messages')
+        await this.sleep(2500)
+        const coords = await page.$eval(`#spyTable .row:nth-child(${spyCount}) a.txt_link`, el => el.innerText)
+        const planet = this.getClosestPlanet(planetsCoordinates, coords)
+        await planet.forcePlanet(page)
+        await this.sleep(2500)
+        await page.click(`#spyTable .row:nth-child(${spyCount}) a.spyTableIcon.icon_attack`)
+        await this.sleep(2500)
+        await page.click('#continue')
+        await this.sleep(2500)
+        await page.click('#continue')
+        await this.sleep(2500)
+        await page.click('#start')
+        await this.sleep(2500)
+        maxFleets--
+      } catch (e) {
+        console.error(`Could not attack n°${spyCount}: ${e}`)
+      }
+      spyCount++
+    }
   }
 }
 
