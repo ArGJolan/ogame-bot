025db7cd545fc3a032865c90f0c3c9085b47f8f5
ArGJolan <argjolan@gmail.com>
2019-03-28 08:37:47 +0100

    Imrpovements & fixes on WD, now notifies on attack

--- scenarios/watchdog.js
+++ scenarios/watchdog.js
@@ -1,26 +1,68 @@
 const Scenario = require('./scenario')
+const config = require('../config')
+const Notifier = require('../classes/notifier')
 
 class WatchDog extends Scenario {
   constructor (planets, researches, fleets) {
     super(planets, researches, fleets)
 
     this.ids = {}
+    this.lastCheck = new Date()
+    this.notifier = new Notifier()
+    this.notifier.notifyAll('Ogame Bot', 'Watchdog started').then(data => console.log('Wathdog started')).catch(e => console.log('Notify may not work properly'))
+
+    this.alerts = {
+      eventWindow: false
+    }
   }
 
   isAppliable () {
     return true
   }
 
-  async loop (page) {
-    const hostilesId = await page.$$eval('#eventContent .hostile', els => {
-      const result = []
-      for (let el of els) {
-        const value = el.id.split('-')[2]
-        result.push(value)
-      }
-      return result
+  async watchDogCheck (page) {
+    const currentMinutes = await page.$eval('#bar .OGameClock > span', el => {
+      return el.innerText.split(':')[1]
     })
-    console.log(hostilesId)
+    const lastRefreshMinutes = await page.$eval('#bar > .ago_clock', el => {
+      return el.innerText.split(' ')[1].split(':')[1]
+    })
+
+    if (!this.nextRefresh) {
+      this.nextRefresh = 10 + Math.floor(Math.random() * 10)
+    }
+
+    if (Math.abs(currentMinutes - lastRefreshMinutes) > this.nextRefresh) {
+      console.log(`Page has not changed in ${this.nextRefresh} minutes, refreshing`)
+      this.nextRefresh = 0
+      await this.sleep(Math.floor(Math.random() * 30))
+      await page.goto(`https://s${config.account.server}-fr.ogame.gameforge.com/game/index.php?page=overview`)
+      await this.sleep(10000)
+      return
+    }
+
+    let hostilesId = []
+    try {
+      hostilesId = await page.$$eval('#eventContent .hostile', els => {
+        const result = []
+        for (let el of els) {
+          const value = el.id.split('-')[2]
+          result.push(value)
+        }
+        return result
+      })
+      this.lastCheck = new Date()
+      this.alerts.eventWindow = false
+    } catch (e) {
+      console.error('Could not check event window')
+      if (new Date() - this.lastCheck > 120000 && !this.alerts.eventWindow) {
+        this.alerts.eventWindow = true
+        console.error('Event windows has not been checked in 2 minutes, something is wrong')
+        await this.notifier.notifyAll('Error', 'Event window check failed in the last 2 minutes')
+      }
+    }
+
+    // console.log(Math.abs(currentMinutes - lastRefreshMinutes), hostilesId)
 
     for (let id of hostilesId) {
       if (!this.ids[id]) {
@@ -35,11 +77,22 @@ class WatchDog extends Scenario {
       })
 
       if (this.ids[id].arrival !== info.arrival) {
-        console.log('Someone is attacking', this.info.comp)
+        console.log('Someone is attacking', info.comp)
+        await this.notifier.notifyAll('►►► ATTACK ◄◄◄', 'Someone is attacking you\r\n' + this.info.comp)
         this.ids[id] = info
       } else {
         console.log(this.ids[id].arrival, info.arrival)
       }
+    }
+  }
+
+  async loop (page) {
+    try {
+      await this.watchDogCheck(page)
+    } catch (e) {
+      const time = new Date()
+      const format = num => `${num < 10 ? '0' : ''}${num}`
+      console.log(`[${format(time.getHours())}:${format(time.getMinutes())}:${format(time.getSeconds())}]\tWatchDog check failed : ${e.message}`)
     }
   }
 }
